fdc14f4d1615ede1653a77d7fdf12889878f96a0
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-01-05 20:05:54 +0800

    Refactor DynamoDatabase class to DbManager; implement methods for item management in DynamoDB.

--- backend/services/aws/dynamo_database.py
+++ backend/services/aws/dynamo_database.py
@@ -1,13 +1,50 @@
-class DynamoDatabase:
+from botocore.exceptions import ClientError
 
-    def get_data(self):
-        pass
+from backend.config.env import env
+from backend.services.aws.session import Session
 
-    def insert_data(self, data):
-        pass
 
-    def update_data(self, data):
-        pass
+class DbManager:
+    def __init__(self):
+        session = Session().get_session()
+        self.dynamodb = session.resource("dynamodb", region_name=env.aws_region)
+        self.table = self.dynamodb.Table(env.table)
 
-    def delete_data(self, data_id):
-        pass
+    def add_item(self, item: dict):
+        return self.table.put_item(Item=item)
+
+    def remove_item(self, data: dict):
+        return self.table.delete_item(Key=data)
+
+    def query_items(self, keys) -> list:
+        try:
+            return self.table.query(
+                Select="ALL_ATTRIBUTES",
+                ConsistentRead=True,
+                KeyConditionExpression=(keys),
+            )["Items"]
+        except ClientError:
+            return []
+
+    def update_item(self, **data):
+        return self.table.update_item(**data)
+
+    def get_item(self, key):
+        try:
+            value = self.table.get_item(Key=key)
+            if "Item" in value:
+                return value["Item"]
+            return None
+        except ClientError:
+            return None
+
+    def batch_get_item(self, keys: list[dict]):
+        try:
+            if not keys:
+                # Return an empty response structure when no keys are provided
+                return {"Responses": {self.table.name: []}, "UnprocessedKeys": {}}
+            return self.dynamodb.batch_get_item(
+                RequestItems={self.table.name: {"Keys": keys}}
+            )
+        except ClientError:
+            return None
