ee352586cb767855f6b9e175acbf4f08bdb52be5
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-01-12 04:47:57 +0800

    Add GraphicDesignerAgent class for handling graphic design tasks; initialize system prompt and model in constructor

--- backend/services/agent/graphic_designer_agent.py
+++ backend/services/agent/graphic_designer_agent.py
@@ -6,17 +6,24 @@ from backend.config.enum import TeamEnum
 from backend.services.ai.open_ai import OpenAI, ModelEnum
 
 
-class ImageAgent(BaseAgent):
+# https://docs.langchain.com/oss/python/integrations/chat/openai#image-generation
+
+
+class GraphicDesignerAgent(BaseAgent):
     name = "Julia"
     role = TeamEnum.GRAPHIC_DESIGNER
     responsibility = "Designing visual content for various media"
     teams = []
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.system_prompt = SystemPromptHelper(
             role=self.role, teams=self.teams
         ).get_system_prompt()
         self.model = OpenAI(ModelEnum.GPT_5).get_model()
+        self.model = self.model.bind_tools(self.__set_image_tools())
+
+    def __set_image_tools(self) -> list[dict]:
+        return [{"type": "image_generation", "quality": "low"}]
 
     def start_task(self, task: str):
         pass
