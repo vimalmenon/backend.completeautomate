ab4d0c67b0e8ec5df076b383e0aebeccf7a5b24e
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-01-18 11:19:59 +0800

    Refactor NewIdeaTask to enable GraphicDesignerAgent and comment out PlannerAgent task initiation

--- backend/services/task/new_idea_task.py
+++ backend/services/task/new_idea_task.py
@@ -1,6 +1,7 @@
 from backend.services.agent.manager_agent import ManagerAgent
 from backend.services.agent.frontend_agent import FrontendAgent
 from backend.services.agent.planner_agent import PlannerAgent
+from backend.services.agent.graphic_designer_agent import GraphicDesignerAgent
 
 
 class NewIdeaTask:
@@ -8,4 +9,5 @@ class NewIdeaTask:
     def input(self, task: str):
         # manager = ManagerAgent().start_task(task=task)
         # FrontendAgent().start_task(task=task)
-        PlannerAgent().start_task(task=task)
+        # PlannerAgent().start_task(task=task)
+        GraphicDesignerAgent().start_task(task=task)
