c7449112f4f2bdb5ba529adcf23b116168995669
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-01-24 12:07:38 +0800

    Add id attribute to Message class; implement delete_message method in MessageDB; update main notebook for message deletion

--- backend/services/aws/message_db.py
+++ backend/services/aws/message_db.py
@@ -11,6 +11,7 @@ class Message:
     messages: list[dict]
     llm_model: str | None
     completed: bool = False
+    id: str | None = None
 
     @classmethod
     def to_cls(cls, data: dict):
@@ -20,10 +21,12 @@ class Message:
             messages=data["messages"],
             completed=data.get("completed", False),
             llm_model=data.get("llm_model", None),
+            id=data.get("id", None),
         )
 
     def to_json(self) -> dict:
         return {
+            "id": self.id,
             "name": self.name,
             "content": self.content,
             "messages": self.messages,
@@ -55,10 +58,17 @@ class MessageDB:
 
     def query_messages(self) -> None:
         try:
-            results =  self.db_manager.query_items(Key(DbKeys.Primary.value).eq(self.table))
+            results = self.db_manager.query_items(
+                Key(DbKeys.Primary.value).eq(self.table)
+            )
             return [Message.to_cls(item) for item in results]
         except Exception as e:
             return []
 
-    def delete_message(self) -> None:
-        pass
+    def delete_message(self, id: str) -> None:
+        self.db_manager.remove_item(
+            {
+                DbKeys.Primary.value: self.table,
+                DbKeys.Secondary.value: id,
+            }
+        )
