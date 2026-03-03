f1513da2f7ff8c2db89e6286f65f9ed8b80e41b9
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-03-03 14:02:02 +0800

    updated platform

--- README.md
+++ README.md
@@ -80,6 +80,7 @@ Use this checklist to track progress toward a **9/10** quality target.
 
 - [x] Rename Task to TaskData 
 - [x] Pass Task as constructor to Jobs
+- [ ] Add Platform data
 - [ ] Agent to review tasks
 - [ ] Need to create platform reference DB ( YouTube or Instagram, Video_id and Channel_id)
 - [ ] Test if it's able to generate YouTube Title, Description and Tags and with multiple options
--- backend/data/platform.py
+++ backend/data/platform.py
@@ -1,16 +1,54 @@
 from dataclasses import dataclass
+from typing import Self
 
 from backend.enum import PlatformEnum
+from backend.exception.app_exception import AppException
+
+
+@dataclass
+class PlatformYouTubeVideoData:
+    channel_id: str
+    video_id: str
+
+    def to_json(self) -> dict:
+        return {"channel_id": self.channel_id, "video_id": self.video_id}
+
+    @classmethod
+    def to_cls(cls, data) -> Self:
+        return cls(channel_id=data["channel_id"], video_id=data["video_id"])
+
+
+@dataclass
+class PlatformYouTubeChannelData:
+    channel_id: str
+
+    def to_json(self) -> dict:
+        return {"channel_id": self.channel_id}
+
+    @classmethod
+    def to_cls(cls, data) -> Self:
+        return cls(channel_id=data["channel_id"])
 
 
 @dataclass
 class PlatformDBData:
-    platform_type: PlatformEnum
-    data: dict
+    platform_type: PlatformEnum | PlatformYouTubeChannelData
+    data: PlatformYouTubeVideoData
 
     def to_json(self) -> dict:
-        return {"data": self.data, "platform_type": self.platform_type.value}
+        return {"data": self.data.to_json(), "platform_type": self.platform_type.value}
 
     @classmethod
-    def to_cls(cls, data):
-        return cls(data=data["data"], platform_type=PlatformEnum(data["platform_type"]))
+    def to_cls(cls, data) -> Self:
+        if data["platform_type"] == PlatformEnum.YouTubeVideo.value:
+            return cls(
+                data=PlatformYouTubeVideoData.to_cls(data["data"]),
+                platform_type=PlatformEnum(data["platform_type"]),
+            )
+        if data["platform_type"] == PlatformEnum.YouTubeChannel.value:
+            return cls(
+                data=PlatformYouTubeChannelData.to_cls(data["data"]),
+                platform_type=PlatformEnum(data["platform_type"]),
+            )
+        else:
+            raise AppException("Not a valid class")
