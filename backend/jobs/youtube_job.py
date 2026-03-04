762d5a2d7931e50034c86d57108f5c44c44ebaea
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-03-04 20:13:01 +0800

    Reorder YouTube task execution to handle ThumbnailUpdater correctly

--- backend/jobs/youtube_job.py
+++ backend/jobs/youtube_job.py
@@ -19,14 +19,14 @@ class YouTubeJob(BaseJob):
 
     def execute(self) -> tuple[TaskStatusEnum, int]:
         try:
-            if self.task.job_type == JobEnum.YouTubeThumbnailUpdater:
-                return (YouTubeThumbnailUpdater(self.task).generate(), 0)
             if self.task.job_type == JobEnum.YouTubeChannel:
                 return (YouTubeChannelCreator(self.task).generate(), 0)
             if self.task.job_type == JobEnum.YouTubeVideo:
                 return (YouTubeVideoCreator(self.task).generate(), 0)
             if self.task.job_type == JobEnum.YouTubeVideoSummarizer:
                 return (YouTubeVideoSummarizer(self.task).generate(), 0)
+            if self.task.job_type == JobEnum.YouTubeThumbnailUpdater:
+                return (YouTubeThumbnailUpdater(self.task).generate(), 0)
             if self.task.job_type == JobEnum.YouTubeVideoMetadataSuggester:
                 return (YouTubeVideoMetadataSuggester(self.task).generate(), 0)
             if self.task.job_type == JobEnum.YouTubeVideoMetadataUpdater:
