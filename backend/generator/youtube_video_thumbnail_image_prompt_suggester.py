f3bef36398ce38e838eac9fcdd126f1a8d72b750
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-03-06 18:02:03 +0800

    feat: implement YouTubeVideoThumbnailImagePromptSuggester with job data initialization

--- backend/generator/youtube_video_thumbnail_image_prompt_suggester.py
+++ backend/generator/youtube_video_thumbnail_image_prompt_suggester.py
@@ -1,8 +1,19 @@
+import logging
+
+from backend.data import YouTubeVideoThumbnailPromptSuggesterJobData
 from backend.enum import TaskStatusEnum
 from backend.generator.base_generator import BaseGenerator
 
+logger = logging.getLogger(__name__)
+
 
 class YoutubeVideoThumbnailImagePromptSuggester(BaseGenerator):
+
+    def __init__(self, task):
+        super().__init__(task)
+        self.job_data = YouTubeVideoThumbnailPromptSuggesterJobData.to_cls(
+            {**task.payload, "task_id": task.id}
+        )
 
     def generate(self) -> TaskStatusEnum:
         return TaskStatusEnum.IN_PROGRESS
