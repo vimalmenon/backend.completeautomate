7f9ae998329666e78101c996820eb7d60f2b1257
Vimal Menon <vimal.webs@gmail.com>
2026-03-07 09:26:51 +0800

    feat: add youtube_channel_factory function and update __all__ exports

--- backend/factory/__init__.py
+++ backend/factory/__init__.py
@@ -1,3 +1,4 @@
 from backend.factory.task_factory import create_task_factory, create_tasks_factory
+from backend.factory.youtube_factory import youtube_channel_factory
 
-__all__ = ["create_task_factory", "create_tasks_factory"]
+__all__ = ["create_task_factory", "create_tasks_factory", "youtube_channel_factory"]
--- backend/factory/youtube_factory.py
+++ backend/factory/youtube_factory.py
@@ -1,7 +1,23 @@
 from faker import Faker
 
+from backend.data import YouTubeChannelDBData
+
 faker = Faker()
 
 
-def youtube_channel_factory():
-    faker.name()
+def youtube_channel_factory() -> YouTubeChannelDBData:
+    return YouTubeChannelDBData(
+        ref_id=faker.str(),
+        title=faker.name(),
+        description=faker.text(),
+        custom_url=faker.url(),
+        published_at=faker.date_time(),
+        last_updated_at=faker.date_time(),
+        country=faker.country(),
+        thumbnail_url=faker.image_url(),
+        banner_image_url=faker.image_url(),
+        privacy_status=faker.word(),
+        made_for_kids=faker.boolean(),
+        task_id=faker.uuid4(),
+        stats=[],
+    )
