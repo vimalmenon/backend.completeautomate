fae0df1e1948563da0c70cf6a60c539c10f42f94
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-03-11 21:34:00 +0800

    refactor: streamline update_task method and remove redundant code

--- backend/database/task/task_db.py
+++ backend/database/task/task_db.py
@@ -28,7 +28,20 @@ class TaskDB:
 
     def update_task(self, task: TaskData):
         logger.info(f"Updating task with id: {task.id}")
-        self.__update_task(task)
+        key = {
+            DbKeysEnum.Primary.value: self.TABLE,
+            DbKeysEnum.Secondary.value: str(task.id),
+        }
+        self.db_manager.update_data(
+            key=key,
+            values={
+                "status": task.status.value,
+                "completed_at": (
+                    task.completed_at.isoformat() if task.completed_at else None
+                ),
+                "failed_count": task.failed_count,
+            },
+        )
 
     def get_tasks(self) -> list[TaskData]:
         logger.info("Fetching all tasks")
@@ -75,55 +88,3 @@ class TaskDB:
             filter_expression=Attr("status").eq(task_status.value),
         )
         return [TaskData.to_cls(item) for item in items]
-
-    def __update_task(self, task: TaskData) -> None:
-        logger.info(
-            f"Updating task status to {task.status.value} for task id: {task.id}"
-        )
-        # key = {
-        #     DbKeysEnum.Primary.value: self.TABLE,
-        #     DbKeysEnum.Secondary.value: str(task.id),
-        # }
-        # self.db_manager.update_data(
-        #     key=key,
-        #     data={
-        #         "status": task.status.value,
-        #         "completed_at": (
-        #             task.completed_at.isoformat() if task.completed_at else None
-        #         ),
-        #         "failed_count": task.failed_count,
-        #     },
-        # )
-
-        update_expression = [
-            "#status = :status",
-            "failed_count = :failed_count",
-            "completed_at = :completed_at",
-        ]
-        expression_attribute_values = {
-            ":status": task.status.value,
-            ":completed_at": (
-                task.completed_at.isoformat() if task.completed_at else None
-            ),
-            ":failed_count": task.failed_count,
-        }
-        self.db_manager.update_item(
-            Key={
-                DbKeysEnum.Primary.value: self.TABLE,
-                DbKeysEnum.Secondary.value: str(task.id),
-            },
-            UpdateExpression=f"SET {', '.join(update_expression)}",
-            ExpressionAttributeNames={"#status": "status"},
-            ExpressionAttributeValues=expression_attribute_values,
-        )
-
-    def update_data(self, key: dict, values: dict) -> None:
-        update_expression, expression_attribute_names, expression_attribute_values = (
-            self.db_manager.get_update_values(values)
-        )
-        self.db_manager.update_item(
-            Key=key,
-            UpdateExpression=update_expression,
-            ExpressionAttributeNames=expression_attribute_names,
-            ExpressionAttributeValues=expression_attribute_values,
-        )
