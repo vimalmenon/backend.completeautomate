8018d854259cd28b73cd01b19d5dd6c9b699c4b7
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-03-20 12:16:48 +0800

    removed YouTubeChannelCreator

--- backend/generator/__init__.py
+++ backend/generator/__init__.py
@@ -1,12 +1,10 @@
 from backend.generator.youtube_channel_creator import (
-    YouTubeChannelCreator,
     YouTubeChannelCreatorJob,
     YouTubeChannelOnboardingJob,
     YouTubeChannelVideoCheckerJob,
 )
 
 __all__ = [
-    "YouTubeChannelCreator",
     "YouTubeChannelCreatorJob",
     "YouTubeChannelVideoCheckerJob",
     "YouTubeChannelOnboardingJob",
--- backend/generator/youtube_channel_creator.py
+++ backend/generator/youtube_channel_creator.py
@@ -19,7 +19,6 @@ from backend.integration.youtube.youtube_api import YouTubeAPI
 from backend.manager import (
     JobManager,
     PlatformManager,
-    TaskManager,
     YouTubeChannelManager,
     YouTubeVideoManager,
 )
@@ -157,7 +156,6 @@ class YouTubeChannelVideoCheckerJob(BaseGeneratorJob):
             MockYouTubeAPI() if env.OFFLINE else YouTubeAPI()
         )
         self.channel_manager = YouTubeChannelManager(ref_id=self.task_data.ref_id)
-        self.manager = TaskManager()
 
     def generate(self) -> tuple[JobsStatusEnum, dict | None]:
         videos = self.youtube_api.list_all_videos(self.task_data.platform.channel_id)
@@ -195,60 +193,3 @@ class YouTubeChannelVideoCheckerJob(BaseGeneratorJob):
         )
         return PlatformManager().save_data(data)
 
-
-class YouTubeChannelCreator(BaseGenerator):
-
-    def __init__(self, task):
-        super().__init__(task)
-        self.youtube_api: YouTubeAPI | MockYouTubeAPI = (
-            MockYouTubeAPI() if env.OFFLINE else YouTubeAPI()
-        )
-        self.job_data = YouTubeJobData.to_cls(self.task.payload)
-        self.manager = TaskManager(task)
-        self.channel_manager = YouTubeChannelManager(ref_id=self.job_data.ref_id)
-
-    def generate(self) -> TaskStatusEnum:
-        channel_from_db = self.channel_manager.get_channel_details()
-        if not channel_from_db:
-            result = self.youtube_api.get_channel_info(
-                self.job_data.platform.channel_id
-            )
-            channel_from_api = YouTubeChannelDBData.to_cls_from_response(
-                {**result, "task_id": str(self.task.id), "ref_id": self.job_data.ref_id}
-            )
-            self.channel_manager.add_channel(channel_from_api)
-            platform_data = self.__get_platform_data()
-            task = self.manager.create_youtube_video_task(
-                ref_id=platform_data.ref_id,
-            )
-            self.manager.add_task(task)
-            logger.info(
-                f"Channel with ID {self.job_data.platform.channel_id} added to database for the first time."
-            )
-        if channel_from_db and channel_from_db.past_update_time(
-            int(self.job_data.poll_frequency_in_days)
-        ):
-            result = self.youtube_api.get_channel_info(
-                self.job_data.platform.channel_id
-            )
-            latest_channel_from_api = YouTubeChannelDBData.to_cls_from_response(
-                {**result, "task_id": str(self.task.id), "ref_id": self.job_data.ref_id}
-            )
-            self.channel_manager.update_channel(
-                latest_channel_from_api.values_to_update(channel_from_db)
-            )
-            logger.info(
-                f"Channel with ID {self.job_data.platform.channel_id} updated in database after polling."
-            )
-        logger.info(
-            f"Channel with ID {self.job_data.platform.channel_id} is up to date in the database. No update needed."
-        )
-        return TaskStatusEnum.IN_PROGRESS
-
-    def __get_platform_data(self) -> PlatformDBData:
-        return PlatformDBData(
-            platform_type=PlatformEnum.YouTubeChannel,
-            data=PlatformYouTubeChannelDBData(
-                channel_id=self.job_data.platform.channel_id
-            ),
-        )
