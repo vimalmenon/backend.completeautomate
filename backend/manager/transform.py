98b2d9de50636d214ef37cd0551f9ba673cecada
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-04-02 07:05:38 +0800

    updated transfrom

--- backend/manager/transform.py
+++ backend/manager/transform.py
@@ -1,47 +1,12 @@
-from backend.database import PromptDB, YouTubeVideoDB
-
-video_mapping = {
-    "Vw_ilJWdzK8": """
-This is the first video in a series of videos created for AI terms.
-In this series of videos, I break down various AI terms, as many of us are not familiar with these terms.
-Ensure the proper part number is given at the end or beginning of the video title as per the best YouTube title convention.
-
-
-The first video link is as follows.
-
-https://youtu.be/Vw_ilJWdzK8
-""",
-    "d4j2OTJdO94": """
-This is the second video in a series of videos created for AI terms.
-In this series of videos, I break down various AI terms, as many of us are not familiar with these terms.
-Ensure the proper part number is given at the end or beginning of the video title as per the best YouTube title convention.
-
-The first video link is as follows.
-https://youtu.be/Vw_ilJWdzK8
-""",
-}
-
-# prompt_mapping = {
-
-# }
+from backend.database import PromptDB
 
 IS_READY = False
 
 
 def transform_data() -> bool:
-    update_videos()
     update_prompt()
     return False
 
-
-def update_videos():
-    videos = YouTubeVideoDB(ref_id="").get_all_videos_from_db()
-    if IS_READY:
-        for video in videos:
-            if user_message := video_mapping.get(video.platform.video_id):
-                YouTubeVideoDB(ref_id=video.ref_id).update_values(
-                    {"user_message": user_message.strip()}
-                )
 
 
 def update_prompt():
