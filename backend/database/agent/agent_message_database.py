0ecd1b897da32b740208d12957b0b6401c7de167
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-04-01 20:32:43 +0800

    remove unused get_all_messages method from AgentMessageDB

--- backend/database/agent/agent_message_database.py
+++ backend/database/agent/agent_message_database.py
@@ -1,4 +1,3 @@
-from boto3.dynamodb.conditions import Key
 
 from backend.data import MessageDBData
 from backend.database.dynamo_database import DbManager
@@ -19,13 +18,6 @@ class AgentMessageDB:
                 **data.to_json(),
             }
         )
-
-    def get_all_messages(self) -> list[MessageDBData]:
-        # TODO remove this
-        messages = self.db_manager.query_items(
-            keys=Key(DbKeysEnum.Primary.value).eq(self.TABLE)
-        )
-        return [MessageDBData.to_cls(message) for message in messages]
 
     def get_messages_by_task_id(self, task_id: str) -> MessageDBData | None:
         item = self.db_manager.get_item(
