b27c5bdcba809c67c0fe9c1818043d9ef14f5748
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-04-13 13:52:57 +0800

    working on youtube short generator

--- backend/jobs/youtube_short_job.py
+++ backend/jobs/youtube_short_job.py
@@ -8,7 +8,16 @@ class YouTubeShortJob(BaseJob):
     types = [JobTypeEnum.YouTubeShortGenerator]
 
     def execute(self) -> JobDataResponse:
-        if self.job.type == JobTypeEnum.YouTubeShortGenerator:
-            status, data = YouTubeShortGenerator().generate()
-            return JobDataResponse(status=status, task_data=data)
-        return JobDataResponse(status=JobsStatusEnum.IN_PROGRESS)
+        try:
+            if self.job.type == JobTypeEnum.YouTubeShortGenerator:
+                status, data = YouTubeShortGenerator().generate()
+                return JobDataResponse(status=status, task_data=data)
+            return JobDataResponse(status=JobsStatusEnum.IN_PROGRESS)
+        except Exception:
+            self.job.failed_count += 1
+            status = (
+                JobsStatusEnum.FAILED
+                if self.job.failed_count >= 4
+                else JobsStatusEnum.IN_PROGRESS
+            )
+            return JobDataResponse(status=status, failed_count=self.job.failed_count)
