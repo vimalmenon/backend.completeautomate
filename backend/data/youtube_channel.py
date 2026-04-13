70a9dd72ac3c5991e6cd8723c68fe28572afd48e
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-04-14 05:18:43 +0800

    fix YouTubeChannelDBData to handle missing playlist data gracefully

--- backend/data/youtube_channel.py
+++ backend/data/youtube_channel.py
@@ -103,7 +103,7 @@ class YouTubeChannelDBData:
     @classmethod
     def to_cls(cls, data: dict) -> Self:
         playlist = [
-            YouTubeChannelPlaylistData.to_cls(playlist) for playlist in data["playlist"]
+            YouTubeChannelPlaylistData.to_cls(playlist) for playlist in data.get("playlist", [])
         ]
         return cls(
             ref_id=data["ref_id"],
