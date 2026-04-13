b998407b598292588d6311e1540fbcc340eb5d89
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-04-14 07:00:56 +0800

    added extra sapce

--- backend/manager/transform.py
+++ backend/manager/transform.py
@@ -5,5 +5,4 @@ def transform_data() -> bool:
     channels = YouTubeChannelManager(ref_id="").get_channels()
     for channel in channels:
         YouTubeChannelManager(ref_id=channel.ref_id).update_channel({"playlist": []})
-
     return False
