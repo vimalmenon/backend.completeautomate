c4f8891a65889feb741d9debf2cae638adc67bdc
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-04-21 13:29:01 +0800

    remove AgentTTSService

--- backend/services/agent_service.py
+++ backend/services/agent_service.py
@@ -77,6 +77,3 @@ class AgentService:
             raise AppException(self.PROMPT_DATA_NOT_FOUND_ERROR)
         return self.__render_template(self.prompt_data.prompt)
 
-
-class AgentTTSService:
-    pass
