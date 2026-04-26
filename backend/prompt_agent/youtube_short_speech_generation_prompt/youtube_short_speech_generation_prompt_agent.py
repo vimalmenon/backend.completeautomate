2137d010461dde185f0f06c3d4e9f37e1ab75177
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-04-27 07:14:52 +0800

    YouTube Video Twitter

--- backend/prompt_agent/youtube_short_speech_generation_prompt/youtube_short_speech_generation_prompt_agent.py
+++ backend/prompt_agent/youtube_short_speech_generation_prompt/youtube_short_speech_generation_prompt_agent.py
@@ -1,16 +1,35 @@
+from dataclasses import dataclass
+
+from backend.ai.text_generation.deepseek_ai import DeepseekAI, ModelEnum
 from backend.data import YouTubeShortDBData
 from backend.enum import PromptTaskEnum
 from backend.prompt_agent.agent.base_agent import BaseAgent
 
 
+@dataclass
+class GraphState:
+    message: list[dict]
+    comment: str
+    iterate: int
+    error: str
+
+
 class YouTubeShortSpeechGenerationPromptAgent(BaseAgent):
     task = PromptTaskEnum.YouTubeShortSpeechGenerationPrompt
+
+    def __init__(self):
+        super().__init__()
+        self.llm = DeepseekAI(model=ModelEnum.DEEPSEEK_REASONER).get_model()
 
     def generate(self, video_short: YouTubeShortDBData):
         self.get_prompt()
 
     def improve(self): ...
 
-    def __test(self): ...
+    def __generate(self): ...
 
     def __reflect(self): ...
+
+    def __format_output(self): ...
+
+    def generate_node(self): ...
--- backend/prompt_agent/youtube_video_twitter_post/youtube_video_twitter_post_agent.py
+++ backend/prompt_agent/youtube_video_twitter_post/youtube_video_twitter_post_agent.py
@@ -1,5 +1,10 @@
+from backend.data import YouTubeVideoDBData
 from backend.enum import PromptTaskEnum
+from backend.prompt_agent.agent.base_agent import BaseAgent
 
 
-class YouTubeVideoTwitterPostAgent:
+class YouTubeVideoTwitterPostAgent(BaseAgent):
     task = PromptTaskEnum.YouTubeVideoTwitterPost
+
+    def generate(self, data: YouTubeVideoDBData):
+        self.get_prompt()
