8c83f1c1e1aba2939b76ff86a5f02f61baa44c69
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-04-27 18:46:33 +0800

    Working on prompt

--- backend/prompt_agent/youtube_short_speech_generation_prompt/youtube_short_speech_generation_prompt_agent.py
+++ backend/prompt_agent/youtube_short_speech_generation_prompt/youtube_short_speech_generation_prompt_agent.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 
-from langgraph.graph import StateGraph
+from langgraph.graph import END, START, StateGraph
 
 from backend.ai.text_generation.deepseek_ai import DeepseekAI, ModelEnum
 from backend.data import YouTubeShortDBData
@@ -35,9 +35,11 @@ class YouTubeShortSpeechGenerationPromptAgent(BaseAgent):
 
     def __reflect(self): ...
 
-    def __format_output(self): ...
+    def __format_output(self, state: GraphState) -> dict:
+        return {}
 
-    def __create_prompt(self, state: GraphState): ...
+    def __create_prompt(self, state: GraphState) -> dict:
+        return {}
 
     def generate_node(self):
         self.__generate_state()
@@ -45,4 +47,9 @@ class YouTubeShortSpeechGenerationPromptAgent(BaseAgent):
     def __generate_state(self):
         builder = StateGraph(GraphState)
         builder.add_node("create_prompt", self.__create_prompt)
-        return builder
+        builder.add_node("format_output", self.__format_output)
+        builder.add_edge(START, "create_prompt")
+        builder.add_edge("create_prompt", "format_output")
+        builder.add_edge("format_output", END)
+
+        return builder.compile()
